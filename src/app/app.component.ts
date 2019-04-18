a927c097481c46a38fb1ddad0e9e496647be4df4
yangzi <ziqiang.yang@mpac.ca>
2019-04-18 11:15:24 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'angular7-registration-login-example-cli-less';
+}
